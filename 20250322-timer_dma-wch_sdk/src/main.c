2f68e2cfb57b01737dff927e57df707541043ac5
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-03-23 20:27:33 +0900

    timer dma

--- 20250322-timer_dma-wch_sdk/src/main.c
+++ 20250322-timer_dma-wch_sdk/src/main.c
@@ -1,7 +1,9 @@
 #include <ch32v00x.h>
 #include <debug.h>
 
-#define LED_PIN GPIO_Pin_2
+#define GPIOA_LED_PIN GPIO_Pin_1
+#define GPIOC_LED_PIN GPIO_Pin_2
+#define GPIOD_LED_PIN GPIO_Pin_4
 
 void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
@@ -13,7 +15,9 @@ void DMA1_Channel2_IRQHandler(void)
     DMA1->INTFCR = DMA1_IT_GL2;
 }
 
-uint32_t gpio_dma_buf[16] = {0};
+uint32_t gpioc_dma_buf[16] = {0};
+uint32_t gpiod_dma_buf[16] = {0};
+uint32_t gpioa_dma_buf[16] = {0};
 
 void init(void)
 {
@@ -26,21 +30,28 @@ void init(void)
     DMA_InitTypeDef DMA_InitStructure = {0};
 
     // GPIOにクロック供給
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_SRAM, ENABLE);
-    // TIM_ARRPreloadConfig(TIM1, ENABLE);
 
     // GPIO出力
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-    GPIO_InitStructure.GPIO_Pin = LED_PIN;
+
+    GPIO_InitStructure.GPIO_Pin = GPIOA_LED_PIN;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = GPIOC_LED_PIN;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = GPIOD_LED_PIN;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
 
     DMA_StructInit(&DMA_InitStructure);
     DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
-    DMA_InitStructure.DMA_BufferSize = sizeof(gpio_dma_buf) / sizeof(gpio_dma_buf[0]);
     DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
     DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
     DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Word;
@@ -48,11 +59,30 @@ void init(void)
     DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
     DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
     DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&GPIOC->BSHR;
-    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)gpio_dma_buf;
+
+    // TIM1_CH1 DMA1_Channel2
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&GPIOA->BSHR;
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)gpioa_dma_buf;
+    DMA_InitStructure.DMA_BufferSize = sizeof(gpioa_dma_buf) / sizeof(gpioa_dma_buf[0]);
     DMA_DeInit(DMA1_Channel2);
     DMA_Init(DMA1_Channel2, &DMA_InitStructure);
     DMA_Cmd(DMA1_Channel2, ENABLE);
+
+    // TIM1_CH2 DMA1_Channel3
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&GPIOC->BSHR;
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)gpioc_dma_buf;
+    DMA_InitStructure.DMA_BufferSize = sizeof(gpioc_dma_buf) / sizeof(gpioc_dma_buf[0]);
+    DMA_DeInit(DMA1_Channel3);
+    DMA_Init(DMA1_Channel3, &DMA_InitStructure);
+    DMA_Cmd(DMA1_Channel3, ENABLE);
+
+    // TIM1_CH3 DMA1_Channel6
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&GPIOD->BSHR;
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)gpiod_dma_buf;
+    DMA_InitStructure.DMA_BufferSize = sizeof(gpiod_dma_buf) / sizeof(gpiod_dma_buf[0]);
+    DMA_DeInit(DMA1_Channel6);
+    DMA_Init(DMA1_Channel6, &DMA_InitStructure);
+    DMA_Cmd(DMA1_Channel6, ENABLE);
 
     // 48MHzで動作するため、48MHz / 480 = 100kHz (10us) でカウント
     TIM_TimeBaseInitStructure.TIM_Prescaler = 480;
@@ -75,6 +105,10 @@ void init(void)
 
     // TIM1_CH1 DMA1_Channel2
     TIM_DMACmd(TIM1, TIM_DMA_CC1, ENABLE);
+    // TIM1_CH2 DMA1_Channel3
+    TIM_DMACmd(TIM1, TIM_DMA_CC2, ENABLE);
+    // TIM1_CH3 DMA1_Channel6
+    TIM_DMACmd(TIM1, TIM_DMA_CC3, ENABLE);
     TIM_DMACmd(TIM1, TIM_DMA_Update, ENABLE);
     TIM_Cmd(TIM1, ENABLE);
 }
@@ -97,13 +131,15 @@ int main(void)
     {
         if (i & 2)
         {
-            gpio_dma_buf[i] = 0xf;
-            // gpio_dma_buf[i] = 0xAA;
+            gpioa_dma_buf[i] = GPIOA_LED_PIN;
+            gpioc_dma_buf[i] = GPIOC_LED_PIN;
+            gpiod_dma_buf[i] = GPIOD_LED_PIN;
         }
         else
         {
-            gpio_dma_buf[i] = 0xf << 16;
-            // gpio_dma_buf[i] = 0x55;
+            gpioa_dma_buf[i] = GPIOA_LED_PIN << 16;
+            gpioc_dma_buf[i] = GPIOC_LED_PIN << 16;
+            gpiod_dma_buf[i] = GPIOD_LED_PIN << 16;
         }
     }
 
@@ -145,9 +181,9 @@ int main(void)
     {
         printf("Step %d %d\r\n", TIM1->CNT, DMA1_Channel2->CNTR);
 
-        // GPIOC->BSHR = LED_PIN;
+        // GPIOC->BSHR = GPIOC_LED_PIN;
         // Delay_Ms(100);
-        // GPIOC->BSHR = LED_PIN << 16;
+        // GPIOC->BSHR = GPIOC_LED_PIN << 16;
         Delay_Ms(1000);
     }
 }
